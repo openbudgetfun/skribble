beed046b00d201ccd55b3aef76f11c3a907d24d5
Ifiok Jr. <ifiokotung@gmail.com>
2026-03-13 11:33:00 +0000

    chore: remove unused import from screenshot test

--- apps/skribble_storybook/integration_test/screenshots_test.dart
+++ apps/skribble_storybook/integration_test/screenshots_test.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:skribble_storybook/app.dart';
